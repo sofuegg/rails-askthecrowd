a2f4ffd07303ea22a0cd9481220a121807f756fe
Zoe Shen <shenyuhang1998917@outlook.com>
2019-08-19 16:31:10 +0800

    my-reponses and asked-questions

---
+++ app/views/api/v1/questions/my_responses.json.jbuilder
@@ -0,0 +1,10 @@
+json.question_lists do
+  json.array! @my_responses do |question|
+    json.extract! question, :id, :title, :photo, :active
+
+    # json.user_info question.user.nickname
+    json.choice_info question.choices
+    json.percentage_one question.statistics[0]
+    json.percentage_two question.statistics[1]
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  post 'user_token' => 'user_token#create'
   root to: 'pages#home'
   post '/login', to: 'login#login'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
+      get "/questions/my_responses/:user_id", to: "questions#my_responses"
+      get "/questions/asked_questions/:user_id", to: "questions#asked_questions"
       resources :questions, only: [ :index, :show, :create ] do
         resources :choices, only: [ :create, :index, :result_to_my_question ] do
           get "/", to: "choices#result_to_my_question"
@@ -13,6 +16,10 @@ Rails.application.routes.draw do
     end
   end
 
+
+  def authenticate_v1_user
+    authenticate_for V1::User
+  end
   # post '/login' to: 'login#login'
 
 end
